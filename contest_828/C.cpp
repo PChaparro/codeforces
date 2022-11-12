91230f95b99e46fdd8e0a9116a909d5bb5631c92
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-11-12 13:53:31 -0500

    :sparkles: Solve `C` problem

--- contest_828/C.cpp
+++ contest_828/C.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,27 +16,40 @@ int main()
 
     while (t--){
         int c;
-        int curr_max = 0;
         char current;
         string tl;
-
         cin >> c >> current >> tl;
 
-        for(int i=0; i<tl.size(); i++){
-            if(tl[i] == current){ // Eval distance
-                int ng = tl.find('g', i) - i;
-                // cout << "Before update: " << ng << endl;
+        if (current == 'g') {
+            cout << 0 << endl;
+        }else{
+            int curr_max = 0;
+            int dist;
+            vector<int> greens;
+            vector<int>::iterator up;
 
-                if (ng < 0){
-                    ng = tl.find('g') + tl.size() - i;
-                }
-
-                // cout << "After update " << ng << endl;
-                curr_max = max(curr_max, ng);
+            for (int i=0; i<tl.size(); i++){ // Get greens indexes
+                if (tl[i] == 'g')
+                    greens.push_back(i);
             }
-        }
 
-        cout << curr_max << endl;
+            for (int i=0; i<tl.size(); i++){ // Get distances
+                if (tl[i] == current){
+                    up = upper_bound(greens.begin(), greens.end(), i);
+
+                    if(up != greens.end())
+                        dist = *up - i;
+                    else{
+                        dist = greens[0] + tl.size() - i;
+                    }
+
+                    curr_max = max(curr_max, dist);
+                    // cout << "After update: " << dist << endl;
+                }
+            }
+
+            cout << curr_max << endl;
+        }
 
     }
 
