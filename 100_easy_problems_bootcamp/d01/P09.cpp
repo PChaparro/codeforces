d9e84b3679509799c52ebafcd04ff84e94f77625
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-03-25 20:33:35 -0500

    feat(d1): P09

---
+++ 100_easy_problems_bootcamp/d01/P09.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// debug util
+#ifdef DEBUG
+	#define deb(a) cerr << #a << "= " << a << endl
+	#define deb2(a,b) cerr << a << " : " << b << endl;
+	#define deb3(a,b,c) cerr << a << " , " << b << " , " << b << endl;
+#else
+	#define de(a)
+	#define de2(a,b)
+	#define de3(a,b,c)
+#endif
+
+// useful
+#define ll long long
+#define lld long double
+#define umap unordered_map
+typedef pair<int,int> pii;
+
+bool multi=false;
+
+void solve(){
+  int n, ec = 0; cin >> n;
+  char cn;
+
+  for(int i = 0; i < n; i++){
+    cin >> cn;
+    if(cn == '8') ec++;
+  }
+
+  cout << min(ec, n / 11) << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t=1;
+    if(multi)cin>>t;
+    while(t--)solve();
+    return 0;
+}
+
