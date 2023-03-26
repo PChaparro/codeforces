55a26b5d033e6b7d61eb93d24c12202e4881f14b
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-03-26 09:49:24 -0500

    feat(d2): P017

---
+++ 100_easy_problems_bootcamp/d02/P17.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// debug util
+#ifdef DEBUG
+	#define deb(a) cerr << #a << "= " << a << endl
+	#define deb2(a,b) cerr << a << " : " << b << endl;
+	#define deb3(a,b,c) cerr << a << " , " << b << " , " << b << endl;
+#else
+	#define de(a)
+	#define de2(a,b)
+	#define de3(a,b,c)
+#endif
+
+// useful
+#define ll long long
+#define lld long double
+#define umap unordered_map
+#define uset unordered_set
+typedef pair<int,int> pii;
+
+bool multi=false;
+
+void solve(){
+  ll a, b, x, y; cin >> a >> b >> x >> y;
+
+  // Reduce the aspect ratio to the min fraction
+  ll gcd = __gcd(x, y);
+  x /= gcd;
+  y /= gcd;
+  // cout << x << " " << y << endl;
+
+  cout << min((a / x), (b / y)) << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t=1;
+    if(multi)cin>>t;
+    while(t--)solve();
+    return 0;
+}
+
