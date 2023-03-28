0997615ca4c74766d90348135a6382fa1ecacbea
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-03-27 20:13:10 -0500

    feat(d03): P25

---
+++ 100_easy_problems_bootcamp/d03/P25.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// debug util
+#ifdef DEBUG
+	#define deb(a) cerr << #a << "= " << a << endl
+	#define deb2(a,b) cerr << a << " : " << b << endl;
+	#define deb3(a,b,c) cerr << a << " , " << b << " , " << b << endl;
+#else
+	#define de(a)
+	#define de2(a,b)
+	#define de3(a,b,c)
+#endif
+
+// useful
+#define ll long long
+#define lld long double
+#define umap unordered_map
+#define uset unordered_set
+typedef pair<int,int> pii;
+
+bool multi=false;
+
+void solve(){
+  int n, cn, mv = -1, mi; cin >> n;
+
+  for(int i = 0; i < n; i++){
+    cin >> cn;
+
+    if(mv == -1 || cn < mv){
+      mv = cn;
+      mi = i;
+    }
+  }
+
+  cout << mi << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t=1;
+    if(multi)cin>>t;
+    while(t--)solve();
+    return 0;
+}
+
